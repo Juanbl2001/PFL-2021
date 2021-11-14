23f7d7df86862b1b214a843a69723b59dde8906f
Filipe <pi.pinho2@hotmail.com>
2021-11-14 23:43:36 +0000

    1.2 done

--- proj1/Fib.hs
+++ proj1/Fib.hs
@@ -3,3 +3,9 @@ fibRec :: (Integral a) => a -> a
 fibRec x = if x < 2 then x else fibRec (x-1) + fibRec (x-2)
 
 --1.2
+fibLista :: (Integral a) => a -> a
+fibLista x = map fibLista' [0 ..] !! x
+    where
+      fibLista' 0 = 0
+      fibLista' 1 = 1
+      fibLista' n = fibLista (n - 1) + fibLista (n - 2)

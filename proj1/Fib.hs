b95bac972855ff0752243d180238d89d152a8c8d
juanbellon <juan.bellon2001@hotmail.com>
2021-11-16 17:05:28 +0000

    Not much

--- proj1/Fib.hs
+++ proj1/Fib.hs
@@ -2,10 +2,18 @@
 fibRec :: (Integral a) => a -> a
 fibRec x = if x < 2 then x else fibRec (x-1) + fibRec (x-2)
 
+
 --1.2
-fibLista :: (Integral a) => a -> a
-fibLista x = map fibLista' [0 ..] !! x
+
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibLista n = fibs !! n
+
+{-
+--1.3
+fibListaInfinita ::  (Integral a) => a -> a
+fibListaInfinita x = map fibListaInfinita' [0 ..] !! x
     where
-      fibLista' 0 = 0
-      fibLista' 1 = 1
-      fibLista' n = fibLista (n - 1) + fibLista (n - 2)
+      fibListaInfinita' 0 = 0
+      fibListaInfinita' 1 = 1
+      fibListaInfinita' x = fibListaInfinita (x - 1) + fibListaInfinita (x - 2)
+-}

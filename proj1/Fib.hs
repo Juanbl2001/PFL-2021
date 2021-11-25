383f91acada9414cd4d28c83ced6e990779fb424
juanbellon <juan.bellon2001@hotmail.com>
2021-11-25 11:45:27 +0000

    finished Fib

--- proj1/Fib.hs
+++ proj1/Fib.hs
@@ -1,21 +1,22 @@
+import Control.Monad.State
 --1.1
 fibRec :: (Integral a) => a -> a
 fibRec x = if x < 2 then x else fibRec (x-1) + fibRec (x-2)
 
 
+--1.2
+
+fibLista :: Integer -> Integer
+fibLista n = flip evalState (0,1) $ do
+  forM_ [0..(n-1)] $ \_ -> do
+    (a,b) <- get
+    put (b,a+b)
+  (a,b) <- get
+  return a
+
 --1.3
 
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-fibLista :: Int -> Integer
-fibLista n = fibs !! n
-
-{-
---1.3
-fibListaInfinita ::  (Integral a) => a -> a
-fibListaInfinita x = map fibListaInfinita' [0 ..] !! x
-    where
-      fibListaInfinita' 0 = 0
-      fibListaInfinita' 1 = 1
-      fibListaInfinita' x = fibListaInfinita (x - 1) + fibListaInfinita (x - 2)
--}
+fibListaInfinita :: Int -> Integer
+fibListaInfinita n = fibs !! n
